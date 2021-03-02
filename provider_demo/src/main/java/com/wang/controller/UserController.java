8ee70dcbc3d727e86643eff7b394fee90e6dcba9
guo-bao-rou <13716946845@163.com>
2021-03-02 23:14:24 +0800

    服务提供  add by wangchao 20210302

--- provider_demo/src/main/java/com/wang/controller/UserController.java
+++ provider_demo/src/main/java/com/wang/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.wang.controller;
 
+import com.wang.mapper.UserMapper;
 import com.wang.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-@RequestMapping("/userController")
+@RequestMapping(value = "userController")
 public class UserController {
 
-    @RequestMapping(value = "/{id}")
-    public User getUser(Long id){
-        User user = new User(id,"2323");
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public User getUser(@PathVariable("id") Long id){
+        User user = userMapper.selectById(id);
         return user;
     }
 }
